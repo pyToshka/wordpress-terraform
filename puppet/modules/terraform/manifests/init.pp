5f7124445bde68f038d9b1b8b8db6470bfc94d7f
Iurii Medvedev <iurii_medvedev@epam.com>
2017-09-27 12:07:59 +0300

    fix Permission denied issue

--- puppet/modules/terraform/manifests/init.pp
+++ puppet/modules/terraform/manifests/init.pp
@@ -4,28 +4,23 @@ class terraform (
     command => "/usr/bin/apt-get update",
   }
 
-  package { "wget":
-    require => Exec["update"],
-  }
-
   exec{'get_terraform':
     command => "/usr/bin/wget -q https://releases.hashicorp.com/terraform/0.10.6/terraform_0.10.6_linux_amd64.zip -O /tmp/terraform.zip",
-    require => Package['wget'];
+    notify  => Exec['unzip_terraform']
   }
 
   exec {'unzip_terraform':
     path => '/usr/bin',
     command => 'sudo unzip /tmp/terraform.zip -d /usr/local/bin',
-    require => File['get_terraform'],
+    require => Exec['get_terraform'],
   }
   exec{'get_packer':
     command => "/usr/bin/wget -q https://releases.hashicorp.com/packer/1.1.0/packer_1.1.0_linux_amd64.zip -O /tmp/packer.zip",
-    require => Package['wget'];
   }
 
   exec {'unzip_packer':
     path => '/usr/bin',
     command => 'sudo unzip /tmp/packer.zip -d /usr/local/bin',
-    require => File['get_packer'],
+    require => Exec['get_packer'],
   }
 }
